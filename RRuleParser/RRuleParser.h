f3a0716ccf2e64fc9871d78aea9292566b185df3
dryErase <beno695@gmail.com>
2017-12-19 15:42:11 -0800

    Initial Commit

---
+++ RRuleParser/RRuleParser.h
@@ -0,0 +1,19 @@
+//
+//  RRuleParser.h
+//  RRuleParser
+//
+//  Created by Benjamin Pust on 12/19/17.
+//  Copyright © 2017 Benjamin Pust. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RRuleParser.
+FOUNDATION_EXPORT double RRuleParserVersionNumber;
+
+//! Project version string for RRuleParser.
+FOUNDATION_EXPORT const unsigned char RRuleParserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RRuleParser/PublicHeader.h>
+
+
